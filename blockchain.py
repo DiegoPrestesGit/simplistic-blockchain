1842c1141632438eed39b75d86d968c690fbb51f
Diego Prestes <diegoprestes.git@hotmail.com>
2020-09-07 17:27:01 -0300

    up n runnin

---
+++ blockchain.py
@@ -0,0 +1,20 @@
+from hashlib import sha256
+
+class Block():
+  data = None
+  hash = None
+  nonce = 0
+  previous_hash = '0' * 64
+
+  def __init__(self, data, number=0):
+    self.data = data
+    self.number = number
+
+class Blockchain():
+  pass
+
+def main():
+  block = Block('Hey you', 1)
+
+if __name__ == '__main__':
+  main()
